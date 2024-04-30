7506b067d1953c798cd15c3789b75257c858c083
Ali Shakeri <ali.sh87873@gmail.com>
2024-04-30 15:07:21 +0330

    initial commit

---
+++ class.py
@@ -0,0 +1,20 @@
+class Point:
+    def __init__(self , x , y):
+        self.x = x
+        self.y = y
+class Circle(Point):
+    def __init__(self,x, y , r):
+        super().__init__(x, y)
+        self.r = r
+    def Area(self):
+        return 2 * 3.14 * (self.r)
+    def perime (self):
+        return 3.14*(self.r*self.r)
+class cylinder(Circle):
+    def __init__(self, x, y, r,h):
+        super().__init__(x, y , r)
+        self.h=h
+    def s(self):
+        return (2 * self.Area()) + (self.perime() * self.h)
+c = cylinder(3,4,5,6)
+print(cylinder.s(c))
